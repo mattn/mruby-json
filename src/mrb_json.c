8310b4d83b978fc6981e06cf208d991499513a5f
mattn <mattn.jp@gmail.com>
2012-12-18 18:51:58 +0900

    fix stringify.

--- src/mrb_json.c
+++ src/mrb_json.c
@@ -2,6 +2,7 @@
 #include <mruby/string.h>
 #include <mruby/array.h>
 #include <mruby/hash.h>
+#include <stdio.h>
 #include "parson.h"
 
 static struct RClass *_class_json;
@@ -18,11 +19,43 @@ mrb_value_to_string(mrb_state* mrb, mrb_value value) {
   case MRB_TT_TRUE:
   case MRB_TT_FALSE:
   case MRB_TT_UNDEF:
+    return mrb_funcall(mrb, value, "to_s", 0, NULL);
   case MRB_TT_STRING:
-    return value;
+    return mrb_funcall(mrb, value, "inspect", 0, NULL);
   case MRB_TT_HASH:
-    return value;
+    {
+      mrb_value str = mrb_str_new_cstr(mrb, "{");
+      mrb_value keys = mrb_hash_keys(mrb, value);
+      int n, l = RARRAY_LEN(keys);
+      for (n = 0; n < l; n++) {
+        mrb_value key = mrb_ary_entry(keys, n);
+        mrb_value enckey = mrb_funcall(mrb, key, "to_s", 0, NULL);
+        enckey = mrb_funcall(mrb, enckey, "inspect", 0, NULL);
+        mrb_str_concat(mrb, str, enckey);
+        mrb_str_cat2(mrb, str, ":");
+        mrb_value obj = mrb_hash_get(mrb, value, key);
+        mrb_str_concat(mrb, str, mrb_value_to_string(mrb, obj));
+        if (n != l - 1) {
+          mrb_str_cat2(mrb, str, ",");
+        }
+      }
+      mrb_str_cat2(mrb, str, "}");
+      return str;
+    }
   case MRB_TT_ARRAY:
+    {
+      mrb_value str = mrb_str_new_cstr(mrb, "[");
+      int n, l = RARRAY_LEN(value);
+      for (n = 0; n < l; n++) {
+        mrb_value obj = mrb_ary_entry(value, n);
+        mrb_str_concat(mrb, str, mrb_value_to_string(mrb, obj));
+        if (n != l - 1) {
+          mrb_str_cat2(mrb, str, ",");
+        }
+      }
+      mrb_str_cat2(mrb, str, "]");
+      return str;
+    }
   default:
     mrb_raise(mrb, E_ARGUMENT_ERROR, "invalid argument");
   }
@@ -31,13 +64,6 @@ mrb_value_to_string(mrb_state* mrb, mrb_value value) {
 
 static mrb_value
 json_value_to_mrb_value(mrb_state* mrb, JSON_Value* value) {
-  mrb_value hash;
-  mrb_value ary;
-  JSON_Object* object;
-  JSON_Array* array;
-  size_t count;
-  int n;
-
   switch (json_value_get_type(value)) {
   case JSONError:
   case JSONNull:
@@ -47,24 +73,31 @@ json_value_to_mrb_value(mrb_state* mrb, JSON_Value* value) {
   case JSONNumber:
     return mrb_float_value(json_value_get_number(value));
   case JSONObject:
-    hash = mrb_hash_new(mrb);
-    object = json_value_get_object(value);
-    count = json_object_get_count(object);
-    for (n = 0; n < count; n++) {
-      const char* name = json_object_get_name(object, n);
-      mrb_hash_set(mrb, hash, mrb_str_new_cstr(mrb, name),
-        json_value_to_mrb_value(mrb, json_object_get_value(object, name)));
+    {
+      mrb_value hash = mrb_hash_new(mrb);
+      JSON_Object* object = json_value_get_object(value);
+      size_t count = json_object_get_count(object);
+      int n;
+      for (n = 0; n < count; n++) {
+        const char* name = json_object_get_name(object, n);
+        mrb_hash_set(mrb, hash, mrb_str_new_cstr(mrb, name),
+          json_value_to_mrb_value(mrb, json_object_get_value(object, name)));
+      }
+      return hash;
     }
-    return hash;
   case JSONArray:
-    ary = mrb_ary_new(mrb);
-    array = json_value_get_array(value);
-    count = json_array_get_count(array);
-    for (n = 0; n < count; n++) {
-      JSON_Value* elem = json_array_get_value(array, n);
-      mrb_ary_push(mrb, ary, json_value_to_mrb_value(mrb, elem));
+    {
+      mrb_value ary;
+      ary = mrb_ary_new(mrb);
+      JSON_Array* array = json_value_get_array(value);
+      size_t count = json_array_get_count(array);
+      int n;
+      for (n = 0; n < count; n++) {
+        JSON_Value* elem = json_array_get_value(array, n);
+        mrb_ary_push(mrb, ary, json_value_to_mrb_value(mrb, elem));
+      }
+      return ary;
     }
-    return ary;
   case JSONBoolean:
     if (json_value_get_boolean(value)) {
       return mrb_true_value();
