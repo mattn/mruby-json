bc2686852e8bc927d1841139beedfa5699598d87
mattn <mattn.jp@gmail.com>
2012-12-19 11:25:30 +0900

    use JSON::xxx instead of JSON.xxx

--- test/json.rb
+++ test/json.rb
@@ -1,33 +1,39 @@
 assert('parse object') do
-  JSON.parse('{"foo": "bar"}') == {"foo"=>"bar"}
+  JSON::parse('{"foo": "bar"}') == {"foo"=>"bar"}
+end
+assert('parse null') do
+  JSON::parse('{"foo": null}') == {"foo"=>nil}
 end
 assert('parse array') do
-  JSON.parse('[true, "foo"]')[1] == "foo"
+  JSON::parse('[true, "foo"]')[1] == "foo"
 end
 assert('stringify boolean') do
-  JSON.stringify(true) == "true"
+  JSON::stringify(true) == "true"
 end
 assert('strnigify object with numeric value') do
-  JSON.stringify({"foo"=>"bar"}) == '{"foo":"bar"}'
+  JSON::stringify({"foo"=>"bar"}) == '{"foo":"bar"}'
 end
 assert('strnigify object with string value') do
-  JSON.stringify({"foo"=> 1}) == '{"foo":1}'
+  JSON::stringify({"foo"=> 1}) == '{"foo":1}'
 end
 assert('stringify object with float value') do
-  JSON.stringify({"foo"=> 2.3}) == '{"foo":2.3}'
+  JSON::stringify({"foo"=> 2.3}) == '{"foo":2.3}'
+end
+assert('stringify object with nil value') do
+  JSON::stringify({"foo"=> nil}) == '{"foo":null}'
 end
 assert('stringify object with boolean key and float value') do
-  JSON.stringify({true=> 3.4}) == '{"true":3.4}'
+  JSON::stringify({true=> 3.4}) == '{"true":3.4}'
 end
 assert('stringify object with object key and float value') do
-  JSON.stringify({{"foo"=> "bar"}=> 1.2}) == '{"{\"foo\"=>\"bar\"}":1.2}'
+  JSON::stringify({{"foo"=> "bar"}=> 1.2}) == '{"{\"foo\"=>\"bar\"}":1.2}'
 end
 assert('stringify empty array') do
-  JSON.stringify([]) == "[]"
+  JSON::stringify([]) == "[]"
 end
 assert('strnigify array with few elements') do
-  JSON.stringify([1,true,"foo"]) == "[1,true,\"foo\"]"
+  JSON::stringify([1,true,"foo"]) == "[1,true,\"foo\"]"
 end
 assert('stringify object with several keys') do
-  JSON.stringify({"foo"=>1, "bar"=> 2}) == '{"foo":1,"bar":2}'
+  JSON::stringify({"foo"=>1, "bar"=> 2}) == '{"foo":1,"bar":2}'
 end
